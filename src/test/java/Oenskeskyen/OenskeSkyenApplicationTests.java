bfb79a451df66b90e584cd39980fa80f5434568e
SpaceMasterCarlsen <96848056+SpaceMasterCarlsen@users.noreply.github.com>
2024-11-14 14:59:14 +0100

    fix

--- src/test/java/Oenskeskyen/OenskeSkyenApplicationTests.java
+++ src/test/java/Oenskeskyen/OenskeSkyenApplicationTests.java
@@ -2,8 +2,10 @@ package Oenskeskyen;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("h2")
 class OenskeSkyenApplicationTests {
 
     @Test
